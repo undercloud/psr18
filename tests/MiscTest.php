18a9f4ed87ce272ad37182fdf7564493266e3d87
undercloud <lodashes@gmail.com>
2019-03-24 12:37:45 +0400

    Add files via upload

--- tests/MiscTest.php
+++ tests/MiscTest.php
@@ -7,6 +7,42 @@ use Undercloud\Psr18\Misc;
 
 final class MiscTest extends TestCase
 {
+    public function testHeadSerializer()
+    {
+        $head = [
+            'Host' => ['google.com'],
+            'Content-Type' => [
+                'application/json',
+                'charset=utf-8'
+            ]
+        ];
+
+        $inline = (
+          "Host: google.com\r\n" .
+          "Content-Type: application/json, charset=utf-8"
+        );
+
+        $this->assertEquals($inline, Misc::serializePsr7Headers($head));
+    }
+
+    public function testUriExtract()
+    {
+        $extract = Misc::extractRelativeUrlComponents('?foo=bar');
+        $expect = ['/','?foo=bar'];
+
+        $this->assertEquals($expect, $extract);
+
+        $extract = Misc::extractRelativeUrlComponents('/?foo=bar');
+        $expect = ['/','?foo=bar'];
+
+        $this->assertEquals($expect, $extract);
+
+        $extract = Misc::extractRelativeUrlComponents('/path/to?foo=bar');
+        $expect = ['/path/to','?foo=bar'];
+
+        $this->assertEquals($expect, $extract);
+    }
+
     public function testRelativeUrl()
     {
         $this->assertTrue(Misc::isRelativeUrl('/path'));
